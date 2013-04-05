00e9e0f7b3727c7cce91cd8400329a53db359e48
Lambert Boskamp <lambert@boskamp-consulting.com.nospam>
2013-04-05 15:53:34 +0200

    Table and view name completion working

--- site-lisp/default.el
+++ site-lisp/default.el
@@ -1,6 +1,7 @@
+(setq idm-dir-home (getenv "emacs_dir"))
 ;; Get path of site-lisp directory in a win32 emacs install
-(setq idm-site-lisp (expand-file-name "site-lisp" (getenv "emacs_dir")))
-;; Created by Rob Christie
+(setq idm-dir-site-lisp (expand-file-name "site-lisp" idm-dir-home))
+;; Credits to Rob Christie
 ;; http://emacsblog.org/2007/01/17/indent-whole-buffer/
 (defun iwb ()
   "indent whole buffer"
@@ -9,18 +10,41 @@
   (indent-region (point-min) (point-max) nil)
   (untabify (point-min) (point-max))
   )
+;; Credits to stackoverflow.com user ExplodingRat
+;; http://stackoverflow.com/questions/9688748/emacs-comment-uncomment-current-line
+(defun comment-or-uncomment-region-or-line ()
+    "Comments or uncomments the region or the current line if there's no active region."
+    (interactive)
+    (let (beg end)
+        (if (region-active-p)
+            (setq beg (region-beginning) end (region-end))
+	  (setq beg (line-beginning-position) end (line-end-position)))
+        (comment-or-uncomment-region beg end)))
 ;; Enable electric-indent in js2-mode
-(add-hook 'js2-mode-hook (lambda()
-			   (electric-indent-mode t)
-			   (electric-pair-mode t)
-			   (setq electric-pair-skip-self t)
-			   ;; create concatenated multiline strings on RET
-			   (local-set-key (kbd "RET") 'js2-line-break)
-			   ;; indent whole buffer on Ctrl+Shift+f
-			   (local-set-key (kbd "C-S-f") 'iwb)
-			   (add-to-list 'js2-additional-externs "uError")
-			   (add-to-list 'js2-additional-externs "uInfo")
-			   ))
+(add-hook
+ 'js2-mode-hook
+ (lambda()
+   (electric-indent-mode t)
+   (electric-pair-mode t)
+   (setq electric-pair-skip-self t)
+   ;; create concatenated multiline strings on RET
+   (local-set-key
+    (kbd "RET")
+    'js2-line-break)
+   ;; indent whole buffer on Ctrl+Shift+f
+   (local-set-key
+    (kbd "C-S-f")
+    'iwb)
+   ;; Uncomment line or region on Ctrl+Shift+7
+   (local-set-key
+    (kbd "C-/")
+    'comment-or-uncomment-region-or-line)
+   ;; Line comment also empty lines
+   (setq comment-empty-lines t)
+   ;; TODO: load IDM symbols from file and add them to js2-additional-externs
+   ;; (add-to-list 'js2-additional-externs "uError")
+   ;; (add-to-list 'js2-additional-externs "uInfo")
+   ))
 ;; Always show file name in frame title
 (setq frame-title-format "%b")
 ;; Never require yes or no style answers
@@ -49,10 +73,24 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories 
-	     (expand-file-name "ac-dict" idm-site-lisp))
+	     (expand-file-name "ac-dict" idm-dir-site-lisp))
+
+(let ((idm-dir-dict (expand-file-name "idmacs-dict" idm-dir-home)))
+  (let ((idm-dir-dict-tables     (expand-file-name "tables"     idm-dir-dict))
+	(idm-dir-dict-views      (expand-file-name "views"      idm-dir-dict))
+	(idm-dir-dict-attributes (expand-file-name "attributes" idm-dir-dict))
+	)
+  
+    (add-to-list 'ac-dictionary-directories idm-dir-dict-tables)
+    (add-to-list 'ac-dictionary-directories idm-dir-dict-views)
+    (add-to-list 'ac-dictionary-directories idm-dir-dict-attributes)
+    )
+  )
+
+(let ((a "b") (b 'a)) (message "Value of b is %s" b))
 (global-auto-complete-mode t)
 (setq ac-modes '(js2-mode))
-(setq ac-auto-start nil)
+(setq ac-auto-start 6)
 
 (add-hook 'js2-mode-hook
 	   (lambda ()
@@ -70,10 +108,10 @@
 	     (setq ac-ignore-case t)
 	     ))
 
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ;; yasnippet
-;; ;; See https://github.com/capitaomorte/yasnippet
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; yasnippet
+;; See https://github.com/capitaomorte/yasnippet
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'yasnippet)
 (yas-reload-all)
 (add-hook 'js2-mode-hook

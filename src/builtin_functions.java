b242193bb36094edcf8a9fa3cc9483294d0896c7
Lambert Boskamp <lambert@boskamp-consulting.com.nospam>
2013-04-12 11:22:31 +0200

    Refactoring constant names

--- src/builtin_functions.java
+++ src/builtin_functions.java
@@ -65,9 +65,9 @@ public class builtin_functions {
 	private static ArrayList go_func_arg_opt = null;
 
 	// GLOBAL CONSTANTS: GENERAL ====================================
-	private final static String SNIPPETS_DIR = "SNIPPETS_DIR";
-	private final static String DICTIONARY_DIR = "DICTIONARY_DIR";
-	private final static String HELP_FILE = "HELP_FILE";
+	private final static String GC_DIR_SNIPPETS = "DIR_SNIPPETS";
+	private final static String GC_DIR_DICTIONARY = "DIR_DICTIONARY";
+	private final static String GC_FILE_HELP = "FILE_HELP";
 
 	// GLOBAL CONSTANTS: REGULAR EXPRESSIONS ========================
 	/**
@@ -84,11 +84,8 @@ public class builtin_functions {
 			+ "  ( \\w+ ( \\s+\\w+)? )    # one or two regular argument words \n"
 			+ "| (   < ([^>]+)     >   )  # OR angle-bracketed optional arg \n"
 			+ "| ( \\[ ([^\\]]+) \\]   )  # OR square bracketed optional arg \n"
-			+ ")                          # end: one argument \n" + ""; // workaround
-																		// for
-																		// Eclipse
-																		// pretty
-																		// printer
+			+ ")                          # end: one argument \n"
+			+ "                    \n"; // workaround for Eclipse pretty printer
 
 	/**
 	 * A constant string containing the regular expression to match exactly one
@@ -108,15 +105,16 @@ public class builtin_functions {
 			+ "(                         # begin: zero or more arguments  \n"
 			+ "\\s* ,? \\s*              # opt. comma surrounded by opt. whitspace \n"
 			+ GC_REGEX_ONE_ARGUMENT
-			//TODO: print() doesn't work yet
-//			+ "\\s* ( \\.{3} )? \\s*     # optional ellipsis for variadic functions \n"
+			// TODO: print() doesn't work yet
+			// +
+			// "\\s* ( \\.{3} )? \\s*     # optional ellipsis for variadic functions \n"
 			+ "\\s* ,? \\s*              # opt. comma surrounded by opt. whitspace \n"
 			+ ")*                        # end: zero or more arguments  \n"
 			+ ")                         # end: signature  excl. parens \n"
 			+ "  \\){1,2} )?             # end: opt. signature incl. parens, workaround uExpandString \n"
 			+ "\\s* ;?                   # opt. whitespace and semicolon \n"
 			+ "( ( \\s* /\\* .*? \\*/ \\s* )+ )? # optional multi-line comment (reluctant quantifier!) \n"
-			+ ""; // workaround for Eclipse pretty printer
+			+ "                    \n"; // workaround for Eclipse pretty printer
 
 	private static void idmacs_trace(String m) {
 		System.err.println(m);
@@ -191,7 +189,7 @@ public class builtin_functions {
 			if (lv_whole_match.trim().equals(gv_func_name)) {
 				idmacs_trace("Ignoring this match"
 						+ " (doesn't look like a function definition)");
-				continue; //====================== with next function
+				continue; // ====================== with next function
 			}
 			// Keep track of number of real matches, ignoring odd ones
 			lv_match_number++;
@@ -326,7 +324,7 @@ public class builtin_functions {
 	 * @throws Exception
 	 */
 	private static void idmacs_builtins_write_snippet() throws Exception {
-		File lo_snippets_dir = mkdirs((String) Par.get(SNIPPETS_DIR));
+		File lo_snippets_dir = mkdirs((String) Par.get(GC_DIR_SNIPPETS));
 		File lo_snippet_file = new File(lo_snippets_dir, gv_func_name);
 		FileOutputStream lo_snippet_fos = new FileOutputStream(lo_snippet_file);
 		PrintWriter lo_snippet_writer = new PrintWriter(lo_snippet_fos);
@@ -399,7 +397,7 @@ public class builtin_functions {
 	 * @throws Exception
 	 */
 	private static void idmacs_builtins_create_dictionary() throws Exception {
-		File lo_dictionary_dir = mkdirs((String) Par.get(DICTIONARY_DIR));
+		File lo_dictionary_dir = mkdirs((String) Par.get(GC_DIR_DICTIONARY));
 		File lo_dictionary_file = new File(lo_dictionary_dir, "js2-mode");
 		FileOutputStream lo_dictionary_fos = new FileOutputStream(
 				lo_dictionary_file);
@@ -434,13 +432,13 @@ public class builtin_functions {
 		Par = new HashMap();
 
 		String lv_snippets_dir = args.length > 0 ? args[0] : ".snippets";
-		Par.put(SNIPPETS_DIR, lv_snippets_dir);
+		Par.put(GC_DIR_SNIPPETS, lv_snippets_dir);
 
 		String lv_dictionary_dir = args.length > 1 ? args[1] : ".dictionary";
-		Par.put(DICTIONARY_DIR, lv_dictionary_dir);
+		Par.put(GC_DIR_DICTIONARY, lv_dictionary_dir);
 
 		String lv_help_file = args.length > 2 ? args[2] : "idmacs_uhelp.txt";
-		Par.put(HELP_FILE, lv_help_file);
+		Par.put(GC_FILE_HELP, lv_help_file);
 
 		idmacs_builtins_open_datasource();
 

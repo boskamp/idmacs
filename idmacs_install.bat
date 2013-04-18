58662df57cdf2b513eb605059fe92c2ff4b95b9e
Lambert Boskamp <lambert@boskamp-consulting.com.nospam>
2013-04-18 07:53:40 +0200

    Add copyright notice to installer

--- idmacs_install.bat
+++ idmacs_install.bat
@@ -1,6 +1,22 @@
+@rem Copyright 2013 Lambert Boskamp
+@rem
+@rem Author: Lambert Boskamp <lambert@boskamp-consulting.com.nospam>
+@rem
+@rem This file is part of IDMacs.
+@rem
+@rem IDMacs is free software: you can redistribute it and/or modify
+@rem it under the terms of the GNU General Public License as published by
+@rem the Free Software Foundation, either version 3 of the License, or
+@rem (at your option) any later version.
+@rem
+@rem IDMacs is distributed in the hope that it will be useful,
+@rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+@rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+@rem GNU General Public License for more details.
+@rem
+@rem You should have received a copy of the GNU General Public License
+@rem along with IDMacs.  If not, see <http://www.gnu.org/licenses/>.
 @echo off
-
-rem Make all variables set in this script local
 setlocal enableextensions enabledelayedexpansion
 
 rem Absolute path of the directory where this script is located,

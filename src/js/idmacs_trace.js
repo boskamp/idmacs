611a47f95a9322e9922f903be7cbb48bfd153f71
Lambert Boskamp <lambert@boskamp-consulting.com.nospam>
2013-08-25 11:36:43 +0200

    Add java.util.logging to idmacs_trace; TODO: better formatter

--- src/js/idmacs_trace.js
+++ src/js/idmacs_trace.js
@@ -21,9 +21,8 @@
  * Records the string passed in Par into a persistent trace,
  * if the job variable $IDMACS_TRACE is not empty.
  *
- * Currently, all messages will be passed to uWarning if the
- * trace is on. However, this approach doesn't work for all
- * scripts, so some trace messages are currently just lost.
+ * All messages will be passed to built-in function uWarning,
+ * and also written to a trace file in IdM's working directory.
  *
  * Parameters:
  *   iv_message -
@@ -35,14 +34,41 @@
  * Returns:
  *   nothing
  */
+
+var go_logger = null;
+
 function idmacs_trace(iv_message){
-    if("%$IDMACS_TRACE%" != "") {
-	// TODO: for some reasons, this logging approach
-	// doesn't generate any messages in the most complex pass:
-	//
-	// Parse Built-In Functions Help File
-	//
-	// Think about switching to java.util.logging
-        uWarning(iv_message);
+    // If tracing has not been activated via job constant IDMACS_TRACE,
+    // do nothing but return immediately.
+    if("%$IDMACS_TRACE%" == "") {
+        return; //=============================================== EXIT
     }
+
+    // Initialize logging via Java Logging API on first call
+    if(go_logger == null) {
+        importClass(Packages.java.util.logging.FileHandler);
+        importClass(Packages.java.util.logging.Formatter);
+        importClass(Packages.java.util.logging.Level);
+        importClass(Packages.java.util.logging.Logger);
+        importClass(Packages.java.util.logging.SimpleFormatter);
+
+        go_logger = Logger.getLogger("org.idmacs");
+        go_logger.setLevel(Level.ALL);
+        var lo_format = new SimpleFormatter();
+
+	// This developer trace file will be created in the IdM runtime's
+	// working directory, typicially "C:\usr\sap\IdM\Identity Center".
+	// It will be overwritten by each run of the job.
+        var lo_handler = new FileHandler("dev_idmacs_job");
+	
+        lo_handler.setFormatter(lo_format);
+        go_logger.addHandler(lo_handler);
+        go_logger.setUseParentHandlers(false);
+    }
+    // uWarning doesn't generate any messages when called
+    // from nested JS function calls. For this reason, log
+    // messages are emitted both via uWarning and via Java Logging API.
+    uWarning(iv_message);
+    go_logger.warning(iv_message);
+
 }//idmacs_trace
